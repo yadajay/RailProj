3d45d3006bf0386381f2f43ef34c47c1f9292e7b
Ajay Yadav <ajay.vns@hotmail.com>
2013-05-31 16:16:25 +0530

    initial Files
    initial Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailProj::Application.config.secret_token = '31e1221f3aaeecd63349b0526c35b8aff9a9f62c9819c7fe5dce802ddf5e14e89a1b43e5d4d4d372ec67806c44b7ed1b4381950f2cbb4df2d1a01a4d5b5c0f9d'
